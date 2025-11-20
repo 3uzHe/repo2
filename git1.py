a82a7b1f1aff239921bd01d065eed03b4f99c6db
3uzHe <134849848+3uzHe@users.noreply.github.com>
2025-11-21 00:12:29 +0800

    Add multiply function and update main execution

--- git1.py
+++ git1.py
@@ -1,5 +1,10 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
 
-print(add(3, 5))
+
+if __name__ == "__main__":
+    print("3 + 5 =", add(3, 5))
+    print("3 * 5 =", multiply(3, 5))
